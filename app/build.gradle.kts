5d9c10120c389fe00baa2796af05b96fb3136196
MrSirJangles <beaustwrt248@gmail.com>
2026-08-20 00:09:56 +0800

    Bump SCRAP-YARD to v0.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.scrapyard.game"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.9.0"
+        versionCode = 10
+        versionName = "0.10.0"
     }
 
     buildTypes {
