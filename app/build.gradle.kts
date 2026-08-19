6b005e0733284016434712f39aa66f38c169230a
MrSirJangles <beaustwrt248@gmail.com>
2026-08-20 00:33:36 +0800

    Bump Android version to v0.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.scrapyard.game"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.14.0"
+        versionCode = 15
+        versionName = "0.15.0"
     }
 
     buildTypes {
