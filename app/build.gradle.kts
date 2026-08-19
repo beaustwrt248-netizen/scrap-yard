1fdc4db40e6630b020dd9100c0db97c524a307b1
MrSirJangles <beaustwrt248@gmail.com>
2026-08-20 00:54:02 +0800

    v0.18: bump Android version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.scrapyard.game"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "0.17.0"
+        versionCode = 18
+        versionName = "0.18.0"
     }
 
     buildTypes {
